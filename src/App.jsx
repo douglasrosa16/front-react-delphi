57d9e54aa4dfb5d4925afeee66dcd57f5a5cb00c
DouglasUnisystem <douglasunisystem@gmail.com>
2021-09-23 07:15:59 -0400

    fix things

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import Header from './component/Header';
+import Routes from './routes/routes';
+
+function App() {
+  return (
+    <>
+      <Header />
+      <Routes />      
+    </>
+  );
+}
+
+export default App;
---
+++ src/routes/routes.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
+
+function Routes() {
+  return (
+    <BrowserRouter>
+      <Switch>        
+        <Route exact path="/users" component={() => <h1>Lista de Usuários</h1>} />
+        <Route exact path="/users/:id" component={() => <h1>Usuário com ID</h1>} />
+        <Route exact path="*" component={() => <h1>Página não encontrada</h1>} />
+      </Switch>
+    </BrowserRouter>
+  )
+}
+
+export default Routes;

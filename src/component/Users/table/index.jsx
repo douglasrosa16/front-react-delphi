5587e4dd347c769a7eeb94fd6b445dd0b191b5e3
DouglasUnisystem <douglasunisystem@gmail.com>
2021-10-01 07:25:59 -0400

    method delete

--- src/component/Users/table/index.jsx
+++ src/component/Users/table/index.jsx
@@ -1,8 +1,14 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import './index.css';
+import api from '../../../services/api';
 
-export default function Table(props){    
+export default function Table(props){  
+    
+    async function handleDelete(id){
+        const response = await api.delete(`users/${id}`);        
+    }
+
     return (
         <div className="container">
             <table className="table-main">
@@ -14,14 +20,21 @@ export default function Table(props){
                 </thead>
                 <tbody>
                 {props.user !== null ? props.user.map(user =>    
-                  <tr key={props.user.ID}>                
+                  <tr key={user.ID}>                
                     <td>{user.ID}</td>
                     <td>{user.USERNAME}</td>
                     <td>{user.STATUS}</td>
                     <td>                       
-                        <Link to={`/editar-usuario/${user.ID}`}>                            
-                            Editar
-                        </Link>
+                        <div>
+                            <Link to={`/editar-usuario/${user.ID}`}>                            
+                                Editar
+                            </Link>                            
+                        </div>
+                        <div>                            
+                            <Link to={'/'} onClick={() => handleDelete(user.ID)}>
+                                Deletar
+                            </Link>
+                        </div>                        
                     </td>
                   </tr>     
                   ) 
@@ -30,6 +43,4 @@ export default function Table(props){
             </table>
         </div>
     )
-
-
 }
